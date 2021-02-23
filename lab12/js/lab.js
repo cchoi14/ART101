393a1e950af0fd38eddfa010158c781bc9f52511
cchoi14 <77122065+cchoi14@users.noreply.github.com>
2021-02-23 12:12:16 +0900

    lab12 bonus

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,32 +1,31 @@
 /**
-* Authors: Johnny Choi <cchoi14@ucsc.edu>
-*      and Tristan Dunlop <tdunlop@ucsc.edu>
+ * Authors: Johnny Choi <cchoi14@ucsc.edu>
+ *      and Tristan Dunlop <tdunlop@ucsc.edu>
  * Created:   23 Feb. 2021
  * License:   Public Domain
  **/
 
-// sortingHat function that gives you your house
+// house descriptions for Bonus #1, information taken from the Harry Potter fanwiki
+var houseDescriptions = [
+  "Gryffindor was founded by Godric Gryffindor and values bravery, daring, nerve, and chivalry. Its emblematic animal is the lion, and its colours are scarlet and gold",
+  "Ravenclaw was founded by Rowena Ravenclaw and values intelligence, knowledge, curiosity, creativity and wit. Its emblematic animal is the eagle, and its colours are blue and bronze",
+  "Slytherin was founded by Salazar Slytherin and values ambition, leadership, self-preservation, cunning and resourcefulness. Its emblematic animal is the serpent, and its colours are emerald green and silver",
+  "Hufflepuff was founded by Helga Hufflepuff and values hard work, dedication, patience, loyalty, and fair play. Its emblematic animal is the badger,and yellow and black are its colours"
+];
+// house array for Bonus #2
+var houses = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"];
+
+// Bonus #2 - refactored sortingHat, now it just returns an index to be used in the anonymous function
 function sortingHat(str) {
-  var len = str.length;
-  var mod = len % 4;
-  if (mod == 0) {
-    return "Griffyndor";
-  }
-  else if (mod == 1) {
-    return "Ravenclaw";
-  }
-  else if (mod == 2) {
-    return "Slytherin";
-  }
-  else if (mod == 3) {
-    return "Hufflepuff";
-  }
+  // Bonus #3 futureproofing against more houses
+  return (str.length % houses.length);
 }
 
-// find element
+// button with anon function
 $('#button').click(function() {
   var name = document.getElementById('input-text').value;
-  var house = sortingHat(name);
-  var outputText = "<p>" + name + "! The Sorting Hat has declared you to be a " + house + "!<p>";
-  $('#output').append(outputText);
+  var index = sortingHat(name); // sortingHat now returns an int (array index)
+  // using .html() so that each press of the button overwrites the previous text
+  $('#output p').html(name + "! The Sorting Hat has declared you to be a " + houses[index] + "!");
+  $('#house-desc p').html(houseDescriptions[index] + " (information courtesy of the Harry Potter fanwiki).");
 })
