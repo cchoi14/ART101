62076fccbace48bf1ea966f448e3a1c8894d8aa5
cchoi14 <77122065+cchoi14@users.noreply.github.com>
2021-02-25 12:09:03 +0900

    lab13 bonus

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -8,29 +8,42 @@
 // FizzBuzz function
 // Loop through numbers 1 to 200, listing them as you go
 function FizzBuzz() {
-  // declare one small string to contain every line of the loop
-  // and one big string to be added via DOM
+  // small string is used to contain every 'line'
   var str = "";
+  // one big string contains the whole thing to be printed via DOM
   var strBig = "";
+  var count = $("#count").val();
+  var f1 = $("#factor1").val();
+  var f2 = $("#factor2").val();
+  var f3 = $("#factor3").val();
+  var f4 = $("#factor4").val();
+  var t1 = $("#text1").val();
+  var t2 = $("#text2").val();
+  var t3 = $("#text3").val();
+  var t4 = $("#text4").val();
 
-  for (var i = 1; i <= 200; i++) {
-    // for every multiple of 3, add "Fizz" to string
-    if ((i % 3) == 0) {
-      str += "Fizz";
+  for (var i = 1; i <= count; i++) {
+    // for every multiple of f1, add t1 to string
+    if ((i % f1) == 0) {
+      str += t1;
     }
-
-    // for every multiple of 5, add "Buzz" to string
-    if ((i % 5) == 0) {
-      str += "Buzz";
+    
+    // for every multiple of f1, add t1 to string
+    if ((i % f2) == 0) {
+      str += t2;
     }
 
     // for every multiple of 7, add "Boom" to string
-    if ((i % 7) == 0) {
-      str += "Boom";
+    if ((i % f3) == 0) {
+      str += t3;
+    }
+
+    if ((i % f4) == 0) {
+      str += t4;
     }
 
     // if none of the above applies, add number
-    if ( str == "" ) {
+    if (str == "") {
       str += i;
     }
 
@@ -45,5 +58,7 @@ function FizzBuzz() {
   // change the contents of output div with DOM
   $("#output p").html(strBig);
 }
-// calls FizzBuzz
-FizzBuzz();
+
+$("#button").click(function() {
+  FizzBuzz();
+})
